3a0b60342ee81e0fe8a7e725a88239da54b8ff5b
dankimhaejun <dan.kimhaejun@gmail.com>
2020-01-22 10:26:44 +0900

    콘솔 확인 추가

--- controllers/v1/loan.controller.js
+++ controllers/v1/loan.controller.js
@@ -13,6 +13,7 @@ const bookLoanCheck = async (req, res, next) => {
 			const result35 = await axios(
 				`${process.env.THIRTY_FIVE}/booksearch?pageno=1&display=10&search_type=detail&manage_code=MJ&search_isbn_issn=${isbn}`,
 			); // 신대도서관 접속
+			console.log('result35 :', result35);
 			const result35Data = result35.data;
 			if (result35Data.RESULT_INFO !== 'SUCCESS' || result35Data.LIST_DATA[0].SEARCH_COUNT === 0) {
 				return res.json(loanResult); // 서버 접속이 원활하지 않거나 , 책이 없을 경우
